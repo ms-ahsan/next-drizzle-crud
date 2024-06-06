90a52c82d48d367fc7bb5ca3fb1e9dc097c598f1
Muhammad Salimy Ahsan <71083634+ms-ahsan@users.noreply.github.com>
2024-06-06 14:58:12 +0700

    server action with drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,9 @@
-import 'dotenv/config';
 import { defineConfig } from 'drizzle-kit';
+import { config } from 'dotenv';
+
+config({
+  path: '.env.local',
+});
 
 export default defineConfig({
   dialect: 'postgresql',
--- server/actions/taskActions.ts
+++ server/actions/taskActions.ts
@@ -9,18 +9,33 @@ export const getData = async () => {
   const data = await db.select().from(tasks);
   return data;
 };
+export const getDetails = async (id: number) => {
+  const data = await db.query.tasks.findFirst({
+    where: eq(tasks.id, id),
+  });
+  return data;
+};
 
 export type Tasks = typeof tasks.$inferInsert;
 
-export const addTask = async (task: Tasks) => {
-  await db.insert(tasks).values(task);
-  revalidatePath('/');
+export const addTask = async ({ title, description }: Tasks) => {
+  try {
+    await db.insert(tasks).values({ title, description }).returning();
+    revalidatePath('/');
+    return { success: `New task has been created` };
+  } catch (err) {
+    return { error: 'Failed to create task' };
+  }
 };
 
 export const deleteTask = async (id: number) => {
-  await db.delete(tasks).where(eq(tasks.id, id));
-
-  revalidatePath('/');
+  try {
+    await db.delete(tasks).where(eq(tasks.id, id));
+    revalidatePath('/');
+    return { success: `Task delete successfully` };
+  } catch (err) {
+    return { error: 'Failed to delete task' };
+  }
 };
 
 export const toggleTask = async (id: number, done: boolean) => {
@@ -39,13 +54,18 @@ export const editTask = async (
   title: string,
   description: string
 ) => {
-  await db
-    .update(tasks)
-    .set({
-      title,
-      description,
-    })
-    .where(eq(tasks.id, id));
+  try {
+    await db
+      .update(tasks)
+      .set({
+        title,
+        description,
+      })
+      .where(eq(tasks.id, id));
 
-  revalidatePath('/');
+    revalidatePath(`/task/${id}`);
+    return { success: `task has been edited` };
+  } catch (err) {
+    return { error: 'Failed to edit task' };
+  }
 };
